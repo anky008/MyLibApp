b2605ff2dbbd9705305bd0a3816e858b3b3e3810
anky008 <ankit.pro.132000@gmail.com>
2019-09-27 09:53:22 +0530

    Book info modified

--- app/src/main/java/com/example/mylibrary/book_info_handler.java
+++ app/src/main/java/com/example/mylibrary/book_info_handler.java
@@ -1,12 +1,21 @@
 package com.example.mylibrary;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.recyclerview.widget.LinearLayoutManager;
+
+import static com.example.mylibrary.R.menu.book_info_menu;
+import static com.example.mylibrary.R.menu.menu_main;
 
 public class book_info_handler extends AppCompatActivity {
     private Toast mToast;
@@ -17,9 +26,38 @@ public class book_info_handler extends AppCompatActivity {
         setContentView(R.layout.book_info);
 
         Intent intent=getIntent();
-        if (intent.hasExtra(Intent.EXTRA_TEXT)) {
-
-        }
+        setActionBar("About Document");
 
     }
+
+    public void setActionBar(String heading) {
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.book_info_toolbar);
+        setSupportActionBar(myToolbar);
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(false);
+        actionBar.setDisplayShowHomeEnabled(false);
+        actionBar.setTitle(heading);
+        actionBar.show();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(book_info_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemThatWasClickedId = item.getItemId();
+        if (itemThatWasClickedId == R.id.read_now) {
+            return true;
+        }
+
+        return true;
+    }
+
+
 }
